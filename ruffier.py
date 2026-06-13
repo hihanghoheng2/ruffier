3252a49fd63f8465cc90bee40686bd651f81803f
LS <luqman.ku@gmail.com>
2026-06-13 10:32:43 +0700

    Update ruffier.py

--- ruffier.py
+++ ruffier.py
@@ -11,26 +11,20 @@ def ruffier_index(P1, P2, P3):
    return (4 * (P1+P2+P3) - 200) / 10
 def neud_level(age):
    norm_age = (min(age, 15) - 7) // 2 
-   result = 21 - norm_age * 1.5
-   return  result 
-
+   result = 21 - norm_age * 1.5 
 def ruffier_result(r_index, level):
    if r_index >= level:
        return 0
    level = level - 4 
-
    if r_index >= level:
        return 1
    level = level - 5 
-
    if r_index >= level:
        return 2
    level = level - 5.5 
-
    if r_index >= level:
        return 3
    return 4 
-
 def test(P1, P2, P3, age):
    if age < 7:
        return (txt_index + "0", txt_nodata) 
